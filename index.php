7f97a8f1210c164e2fcdd0d5f3cf18722e59f3ab
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2017-02-23 16:48:28 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <link href="main.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+  </head>
+  <body>
+    <div class="center_page">
+      <h1 class="title">Projects:</h1>
+      <div class="links">
+        <?=$thelist?>
+      </div>
+    </div>
+  </body>
+</html>
